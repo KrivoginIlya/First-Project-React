005944d8fdfe1f134355817cc5b0f47daf4e6acf
unknown <xline174@gmail.com>
2022-02-15 10:40:46 +0300

    last update lesson 2

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { App } from "./App";
-import { AppHooks } from "./app-hooks";
+import {App} from "./App";
+import {AppHooks} from "./app_hooks";
 
 class Test extends React.Component {
   state = {
     isVisible: true,
   };
   toogle = () => {
-    this.setState({ isVisible: !this.state.isVisible });
+    this.setState({isVisible: !this.state.isVisible});
   };
   render() {
-    const { isVisible } = this.state;
+    const {isVisible} = this.state;
     return (
       <div>
         <button onClick={this.toogle}>Toogle</button>
